9336786a10f1709192495e4f8619c36208bfd40c
David Gwilt <davegwilt@gmail.com>
2020-04-16 16:00:35 +0100

    End of lesson saving

--- Game.py
+++ Game.py
@@ -1,14 +1,23 @@
 
 class Game:
 
+    EMPTY = " "
+    DIM = 3
+    P1 = "o"
+    P2 = "x"
+
     def __init__(self):
-        pass
+        self._board = [[Game.EMPTY for _ in range(Game.DIM)] for _ in range(Game.DIM)]
+        self._player = Game.P1
 
     def __repr__(self):
         return ""
 
     def play(self,row,col):
-        pass
+        row -= 1
+        col -= 1
+        self._board[row][col] = self._player
+        self._player = Game.P2 if self._player is Game.P1 else Game.P1
     
     @property
     def winner(self):
